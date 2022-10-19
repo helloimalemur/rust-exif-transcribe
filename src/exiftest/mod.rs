2b68e9aa24977c4ff4f573065827c731f6ac8c23
James Koonts <jkoontsiii@gmail.com>
2022-10-18 21:28:19 -0400

    init

--- src/exiftest/mod.rs
+++ src/exiftest/mod.rs
@@ -1,20 +1,22 @@
-
-use std::*;
+use exif::{Error, Exif, Reader};
 use std::path::Path;
-use exif::Reader;
+use std::*;
 
 pub fn test_grab_exif_and_print() {
     let p = Path::new("./");
 
     for path in &["./image.NEF"] {
-
         let file = std::fs::File::open(path).unwrap();
         let mut bufreader = std::io::BufReader::new(&file);
         let exifreader = exif::Reader::new();
-        let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+        let mut exif = exifreader.read_from_container(&mut bufreader).unwrap();
         for f in exif.fields() {
-            println!("{} {} {}",
-                     f.tag, f.ifd_num, f.display_value().with_unit(&exif));
+            println!(
+                "{} {} {}",
+                f.tag,
+                f.ifd_num,
+                f.display_value().with_unit(&exif)
+            );
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::path::PathBuf;
 use std::env::*;
+use std::path::PathBuf;
 // mod imp;
 // use imp::*;
 use exiftest::*;
 mod exiftest;
 
-
 fn main() {
-
     test_grab_exif_and_print()
     // ar();
-
 }
