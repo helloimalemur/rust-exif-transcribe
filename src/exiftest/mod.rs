72fccfdd22dbec95aebe777dde1b47f71c778490
foxx <jkoontsiii@gmail.com>
2022-11-24 23:02:41 -0500

    .

--- src/exiftest/mod.rs
+++ src/exiftest/mod.rs
@@ -1,6 +1,8 @@
-use exif::{Error, Exif, Reader};
-use std::path::Path;
+use exif::*;
+use std::path::*;
 use std::*;
+use std::borrow::Borrow;
+// use hex::*;
 
 pub fn test_grab_exif_and_print() {
     let p = Path::new("./");
@@ -11,12 +13,21 @@ pub fn test_grab_exif_and_print() {
         let exifreader = Reader::new();
         let mut exif = exifreader.read_from_container(&mut bufreader).unwrap();
         for f in exif.fields() {
-            println!(
-                "{} {} {}",
-                f.tag,
-                f.ifd_num,
-                f.display_value().with_unit(&exif)
-            );
+
+            if f.tag.to_string().eq("UserComment") {
+                println!("{} {}", f.tag.to_string(), f.display_value().with_unit(&exif));
+
+                // let decoded_string = hex::decode("48656c6c6f20776f726c6421");
+                // let decoded_string = hex::decode();
+                //
+                // unsafe { println!("{}", String::from_utf8_unchecked(decoded_string.unwrap())); }
+            }
+            // println!(
+            //     "{} {} {}",
+            //     f.tag,
+            //     f.ifd_num,
+            //     f.display_value().with_unit(&exif)
+            // );
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::env::*;
-use std::path::PathBuf;
-// mod imp;
-// use imp::*;
-use exiftest::*;
 mod exiftest;
+use exiftest::*;
 
 fn main() {
     test_grab_exif_and_print()
